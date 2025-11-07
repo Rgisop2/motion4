5e5d0e4c06af3684e5897d3e7a6b105afa0ed8af
Rgisop2 <160823869+Rgisop2@users.noreply.github.com>
2025-11-07 06:57:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002073865889"))
 ADMINS = int(environ.get("ADMINS", "1327021082"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = environ.get("DB_URI", "mongodb+srv://poulomi6969:hello@cluster.pmevjiz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = environ.get("DB_URI", "mongodb+srv://satyadipdas24_db_user:HtkJpIxhDM3h1qKh@cluster0.o35rffc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = environ.get("DB_NAME", "vjlinkchangerbot")
